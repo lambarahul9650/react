05d0c3a6af8b851204655e3e5a5a3a2dc3e4a082
Rahul Lamba <lambarahul298@gmail.com>
2023-07-13 11:24:47 +0530

    pdated

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://google.com"
+          //href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
