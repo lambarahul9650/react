113ffce94e274a8eeb095a3d64455a101d2e1700
Rahul Lamba <lambarahul298@gmail.com>
2023-07-13 14:02:59 +0530

    pdated

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          //href="https://google.com"
+          //href="https://intellimis.com"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
